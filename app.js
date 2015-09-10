6e2a2779f24017cc8127d7330465209e3d83848d
yangli1990 <yangli1990@live.com.au>
2015-09-10 21:46:57 +1000

    add activate lab feature

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ var herokuPing = require('heroku-ping-alive');
 
 var port = process.env.PORT || 3000;
 
-function createRoute(slide){
+function createRoute(slide, active){
     app.get(slide, function(req, res){
-        res.send(createPage.serve(slide));
+        res.send(active ? createPage.serve(slide) : 'This lab isn\'t active yet');
     });
 }
 
 app.use('/static', express.static(process.cwd() + '/static'));
 
 for (var slide in config.slides){
-    createRoute(slide, config.slides[slide].file);
+	createRoute(slide, config.slides[slide].active);
 }
 
 app.listen(port);
